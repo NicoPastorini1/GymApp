6d7fc8e3b9ae38ea982bd74f692aa82eb0b1cce0
Nicolas <nicopastorini20@gmail.com>
2022-11-16 10:37:25 -0300

    test ambiente

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCXt21qXqswXHCh_EvSxQqY0bVkOTVgG3k",
+    authDomain: "gymapp-43299.firebaseapp.com",
+    projectId: "gymapp-43299",
+    storageBucket: "gymapp-43299.appspot.com",
+    messagingSenderId: "671287489438",
+    appId: "1:671287489438:web:94b597e6009ff8969adc8f",
+    measurementId: "G-1EQ5KL9E61"
+  }
 };
